f392264b5cdba1c6667b3c9c91dc5331eb7371e3
Victor Aremu <me@victorare.mu>
2024-04-14 18:25:49 +0100

    refactor: fmt

--- src-tauri/src/command.rs
+++ src-tauri/src/command.rs
@@ -12,7 +12,9 @@ static INIT: Once = Once::new();
 pub fn init(app_handle: tauri::AppHandle) {
     INIT.call_once(|| {
         swizzle_to_menubar_panel(&app_handle);
+
         update_menubar_appearance(&app_handle);
+
         setup_menubar_event_listeners(&app_handle);
     });
 }
@@ -20,5 +22,6 @@ pub fn init(app_handle: tauri::AppHandle) {
 #[tauri::command]
 pub fn show_menubar_panel(app_handle: tauri::AppHandle) {
     let panel = app_handle.get_panel("main").unwrap();
+
     panel.show();
 }
