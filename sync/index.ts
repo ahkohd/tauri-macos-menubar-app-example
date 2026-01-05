f58942a9399c8f5b809f6b5c000a5b6ff44f60c4
Saxon Fletcher <saxonafletcher@gmail.com>
2026-01-05 22:15:50 +1000

    remove

--- sync/index.ts
+++
@@ -1,8 +0,0 @@
-// Main schema exports
-
-export * from "./types"
-export * from "./columns"
-export * from "./enums"
-export * from "./functions"
-export * from "./table"
-export * from "./to-postgres"
